56271a0355c3f4b83588115fdbdf36d10a1edc29
Fauzan Fadhillah Arisandi <122199835+Shelford21@users.noreply.github.com>
2025-12-27 15:15:23 +0700

    Update skm2pragponly.py

--- skm2pragponly.py
+++ skm2pragponly.py
@@ -25,7 +25,7 @@ def load_sheet():
     """Read Google Sheet (cached for 60 seconds)."""
     return conn.read(worksheet=url)
 #url = "https://docs.google.com/spreadsheets/d/1dK2tKeeRGAiVc6p0guapTITane-NckvuAFB3rrHu3k8/edit?usp=sharing"
-url = "aa"
+url = "asa"
 urlp = "percobaan"
 
 if "submitted" not in st.session_state:
@@ -251,6 +251,7 @@ if admin_password == ADMIN_PASSWORD:
 else:
     if admin_password != "":
         st.error("❌ Incorrect password.")
+
 
 
 
